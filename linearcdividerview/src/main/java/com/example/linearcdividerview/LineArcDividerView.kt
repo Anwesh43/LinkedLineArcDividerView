f90c4f75438e717071bebe41df9d89ea1857722f
Anwesh43 <anweshthecool0@gmail.com>
2020-12-20 05:28:42 +0530

    adding LineArcDividerView class

--- linearcdividerview/src/main/java/com/example/linearcdividerview/LineArcDividerView.kt
+++ linearcdividerview/src/main/java/com/example/linearcdividerview/LineArcDividerView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawLADNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineArcDivider(scale, w, h, paint)
 }
+
+class LineArcDividerView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
