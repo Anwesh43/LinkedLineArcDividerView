0cccc5996b540697f2a3de77f70dc85c6ba2fc0f
Anwesh43 <anweshthecool0@gmail.com>
2020-12-20 05:13:33 +0530

    adding constants and libraries

--- linearcdividerview/src/main/java/com/example/linearcdividerview/LineArcDividerView.kt
+++ linearcdividerview/src/main/java/com/example/linearcdividerview/LineArcDividerView.kt
@@ -1,2 +1,27 @@
 package com.example.linearcdividerview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#3F51B5",
+    "#4CAF50",
+    "#03A9F4",
+    "#795548"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 2
+val arcs : Int = 4
+val scGap : Float = 0.02f / (parts + 1)
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val deg : Float = 60f
